5f693d7abd9bc80a13719d4ec0bb1e66d3d64e7c
Xiaohong Zhu <xzhu0706@gmail.com>
2019-10-05 20:54:14 -0400

    Updates

---
+++ precache-manifest.90b1261fab67a94dfbc85e61b8ce96f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b708c828f5f8bd55ec1080f309c3ba42",
+    "url": "/react-address-book-app/index.html"
+  },
+  {
+    "revision": "a9aa1c206a4de83eb867",
+    "url": "/react-address-book-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "89c77e3e4bf27fd1758a",
+    "url": "/react-address-book-app/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "a9aa1c206a4de83eb867",
+    "url": "/react-address-book-app/static/js/2.168f5366.chunk.js"
+  },
+  {
+    "revision": "89c77e3e4bf27fd1758a",
+    "url": "/react-address-book-app/static/js/main.95312420.chunk.js"
+  },
+  {
+    "revision": "2b6fcdf40e791dbaf015",
+    "url": "/react-address-book-app/static/js/runtime-main.3fbc6a2d.js"
+  }
+]);
